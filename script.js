ee920e837dd6849adaa4551644384aa032f744e6
Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>
2023-02-11 18:39:27 +0200

    Adding comments with examples

---
+++ script.js
@@ -0,0 +1,62 @@
+// Part 1: Arrays
+/*
+let arr = [0, 1, 2, 3, 4, 5]
+console.log(arr[0]);
+console.log(arr[3]);
+
+arr.push(6);
+console.log(arr[6]);
+
+arr.pop();
+console.log(arr);
+
+const arr = [5, 7, 6, 9]
+arr.push(8);
+console.log(arr);
+arr.pop();
+arr = ["Manchester UTD", "Barcelona"];
+*/
+// Part 2: Loops
+/*
+// 2.1: for loops
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+// 2.2: while loops
+let i = 0;
+while(i < arr.length) {
+    console.log(arr[i]);
+    i++;
+}
+
+// 2.3: foreach loops
+// 2.3.1: i of arr
+for(let i of arr) {
+    console.log(i);
+}
+// 2.3.2: arr.forEach
+arr.forEach(i => {
+    console.log(i);
+});
+*/
+
+// Part 3: Objects
+/*
+let Atwa = {Name: "Ahmed Atwa", age: 20, Concentration: "Computers and Systems Engineering"};
+console.log(Atwa.Name);
+console.log(Atwa.age);
+console.log(Atwa.Concentration);
+Atwa.favoriteColor = "red";
+console.log(Atwa.favoriteColor);
+console.log(Atwa);
+*/
+
+/*
+let btns = document.getElementsByClassName("btn");
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].classList.add("deactivated");
+}
+*/
